c4df4f9e518a1c669702e7526d79efb6ca78b3d7
carmonalanzasalvaro <119354379+carmonalanzasalvaro@users.noreply.github.com>
2025-01-29 19:47:57 +0100

    Finalización ejercicio 1 sin corregir

--- Introduccionjava/Programa1/Main.java
+++ Introduccionjava/Programa1/Main.java
@@ -1,6 +1,6 @@
 package Introduccionjava.Programa1;
 
-class Main {
+public class Main {
     public static void main(String[] args) {
         CalculadoraDeEdad calculadora = new CalculadoraDeEdad();
 
